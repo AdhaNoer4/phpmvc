f8fd28dedfea3c1d5b1fc121a553447ca36f2f38
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2023-06-05 14:42:45 +0700

    02 MVC PHP Routing

--- app/core/App.php
+++ app/core/App.php
@@ -3,6 +3,17 @@ class App
 {
     public function __construct()
     {
-        echo "OK!";
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    public function parseURL() // method untuk memecah url
+    {
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/'); // rtrim untuk menghapus tanda "/" diakhir
+            $url = filter_var($url, FILTER_SANITIZE_URL); // untuk membersihkan url dari karakter2 aneh
+            $url = explode('/', $url); // untuk memecah string url menjadi array berdasarkan tanda /
+            return $url;
+        }
     }
 }
